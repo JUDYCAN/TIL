d4aeab260530eedd7977e605c4154346f4d2d613
JUDYCAN <judycanada11@gmail.com>
2025-09-27 15:17:28 +0900

    [D2] Title: 두 개의 숫자열, Time: 59 ms, Memory: 53,760 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -1,5 +1,6 @@
 
 from collections import deque
+
 T = int(input())
 
 for tc in range(1, T+1):
@@ -7,19 +8,14 @@ for tc in range(1, T+1):
     arr1 = deque(map(int, input().split()))
     arr2 = deque(map(int, input().split()))
     final_list = []
-    if N < M:
-        while len(arr1) <= M:
-            sumi = 0
-            for i in range(len(arr1)):
-                sumi += arr1[i] * arr2[i]
-            final_list.append(sumi)
-            arr1.appendleft(0)
-    else:
-        while len(arr2) <= N:
-            sumi = 0
-            for i in range(len(arr2)):
-                sumi += arr1[i] * arr2[i]
-            final_list.append(sumi)
-            arr2.appendleft(0)
+    if N > M:
+        N, M = M, N
+        arr1, arr2 = arr2, arr1
+    while len(arr1) <= M:
+        sumi = 0
+        for i in range(len(arr1)):
+            sumi += arr1[i] * arr2[i]
+        final_list.append(sumi)
+        arr1.appendleft(0)
 
     print(f"#{tc} {max(final_list)}")
